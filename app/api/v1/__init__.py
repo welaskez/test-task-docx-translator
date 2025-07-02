0917537e9c2f810f78cabdef121f7a560c7fb568
welaskez <nazar.nikitenko.03@mail.ru>
2025-07-02 20:52:22 +0500

    feat: created translates router

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,4 +2,7 @@ __all__ = ("router",)
 
 from fastapi import APIRouter
 
+from .translates import router as translates_router
+
 router = APIRouter(prefix="/v1")
+router.include_router(translates_router)
