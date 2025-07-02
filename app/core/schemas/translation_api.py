49047318ac53dc1396f4c0d4a6a42bba7f3f4054
welaskez <nazar.nikitenko.03@mail.ru>
2025-07-02 21:44:17 +0500

    feat: added translation body schema
    - added healthcheck method

--- app/core/schemas/translation_api.py
+++ app/core/schemas/translation_api.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
 
+class TranslateBody(BaseModel):
+    text: str
+
+
 class TranslateResponse(BaseModel):
     translated_text: str
--- app/services/translation_api.py
+++ app/services/translation_api.py
@@ -1,6 +1,8 @@
 from core.enums.trasnaltes import TranslationType
-from core.schemas.translation_service import TranslateResponse
+from core.schemas.translation_api import TranslateBody, TranslateResponse
+from fastapi import HTTPException
 from httpx import AsyncClient
+from starlette import status
 
 
 class TranslationAPI:
@@ -8,6 +10,18 @@ class TranslationAPI:
         self._base_url = base_url
         self._client = AsyncClient(base_url=base_url)
 
-    async def translate(self, translation_type: TranslationType) -> TranslateResponse:
-        response = await self._client.post(url=f"/translate/{translation_type}")
+    async def translate(self, translation_type: TranslationType, text: str) -> TranslateResponse:
+        await self.healthcheck()
+        response = await self._client.post(
+            url=f"/translate/{translation_type.value}/",
+            json=TranslateBody(text=text).model_dump(),
+            timeout=15.0,
+        )
         return TranslateResponse.model_validate(response.json())
+
+    async def healthcheck(self) -> None:
+        response = await self._client.get(url="/")
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=status.HTTP_502_BAD_GATEWAY, detail="Translation service is currently unavailable"
+            )
